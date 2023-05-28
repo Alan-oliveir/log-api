be6fc10c73196c4b37fe0f30b04b7d986361e5ff
Alan-oliveir <alandeog@poli.ufrj.br>
2023-05-28 12:52:40 -0300

    create clienteRepository

--- src/main/java/com/works/log/api/controller/ClienteController.java
+++ src/main/java/com/works/log/api/controller/ClienteController.java
@@ -2,26 +2,22 @@ package com.works.log.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.works.log.domain.model.Cliente;
+import com.works.log.domain.repossitory.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-
-		
-		return manager.createQuery("from Cliente", Cliente.class)	
-			.getResultList();
+		return clienteRepository.findAll();
 	}
 
 }
